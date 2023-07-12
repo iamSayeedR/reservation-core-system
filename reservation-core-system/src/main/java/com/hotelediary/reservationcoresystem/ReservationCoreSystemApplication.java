07a5383a0dc4ea68312794993ffc7866d696de69
IamSayeed <69970789+iamSayeedR@users.noreply.github.com>
2023-07-12 18:48:29 +0530

    Add files via upload

---
+++ reservation-core-system/src/main/java/com/hotelediary/reservationcoresystem/ReservationCoreSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hotelediary.reservationcoresystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationCoreSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationCoreSystemApplication.class, args);
+	}
+
+}
---
+++ reservation-core-system/src/test/java/com/hotelediary/reservationcoresystem/ReservationCoreSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelediary.reservationcoresystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationCoreSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
